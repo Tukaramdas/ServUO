fb35340fe1cb3d370b86edb961c91a97604fa586
Tuka <tim@tukaram.net>
2016-02-25 19:35:25 +0800

    Periodic Update and New Stat Ball

--- Scripts/Customs/Quests/Old Man Quest/Vendor/SBFacialVendor.cs
+++ Scripts/Customs/Quests/Old Man Quest/Vendor/SBFacialVendor.cs
@@ -2,6 +2,64 @@ using System;
 using System.Collections.Generic;
 using Server.Items;
 
+
+namespace Server.Mobiles
+{
+    public class SBFacialVendor : SBInfo
+    {
+        private readonly List<GenericBuyInfo> m_BuyInfo = new InternalBuyInfo();
+        private readonly IShopSellInfo m_SellInfo = new InternalSellInfo();
+        public SBFacialVendor()
+        {
+        }
+
+        public override IShopSellInfo SellInfo
+        {
+            get
+            {
+                return this.m_SellInfo;
+            }
+        }
+        public override List<GenericBuyInfo> BuyInfo
+        {
+            get
+            {
+                return this.m_BuyInfo;
+            }
+        }
+
+        public class InternalBuyInfo : List<GenericBuyInfo>
+        {
+            public InternalBuyInfo()
+            {
+                Add(new GenericBuyInfo("Moisturizer", typeof(Moisturizer), 100, 20, 0xE2b, 0)); 
+
+            }
+        }
+
+        public class InternalSellInfo : GenericSellInfo
+        {
+            public InternalSellInfo()
+            {
+                Add(typeof(Moisturizer), 3);
+
+
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+/*
+ * using System;
+using System.Collections.Generic;
+using Server.Items;
+
 namespace Server.Mobiles
 {
     public class SBFacialVendor : SBInfo
@@ -32,7 +90,8 @@ namespace Server.Mobiles
             public InternalBuyInfo()
             {
                 if (Core.AOS)
-                    this.Add(new GenericBuyInfo(typeof(MonkRobe), 136, 20, 0x2687, 0x21E));
+            //        this.Add(new GenericBuyInfo(typeof(MonkRobe), 136, 20, 0x2687, 0x21E));
+                    this.Add(new GenericBuyInfo(typeof(Moisturizer), 100, 20, 0xE2b, 0)); 
             }
         }
 
@@ -40,7 +99,9 @@ namespace Server.Mobiles
         {
             public InternalSellInfo()
             {
+                Add(typeof(Moisturizer), 3);
             }
         }
     }
 }
+*/
